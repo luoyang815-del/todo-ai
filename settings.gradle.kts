415f3a28328c3065ce989b84d1f09e250ec14295
luoyang815-del <luoyang815@gmail.com>
2025-11-02 14:42:31 +0800

    up

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,19 @@
-rootProject.name = "PocketAssistant"
+pluginManagement {
+    repositories {
+        // 这三行是关键：让插件解析能在 Google / MavenCentral / PluginPortal 找到
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "todo-ai"   // 或保留你现有的名字
 include(":app")
